a5470d120b0057c357d2c414c608c7e354cfff4d
Boris Teplitskiy <123800733+borisTL@users.noreply.github.com>
2024-10-24 11:40:25 +0300

    Time: 15 ms (50.31%), Space: 11.7 MB (17.75%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
+    
+        size1=len(word1)
+        size2=len(word2)
+
+        word3=''
+        min_size=min(size1,size2)
+
+        for i in range(min_size):
+            word3+=word1[i]+word2[i]
+            
+        word3 += word1[min_size:] + word2[min_size:]
+        return word3
         
